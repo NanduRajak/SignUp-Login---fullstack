1b8fb5ea22d2724b4fa9d30dc21a7aae78f90b78
Nandu Rajak <golanukondanandu@gmail.com>
2025-05-05 22:29:53 +0530

    middleware added

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,11 +1,16 @@
-export default function UserProfile({ params }: any) {
+export default async function UserProfile({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = await Promise.resolve(params);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
       <h1>Profile</h1>
       <hr />
       <p className="text-4xl items-center">
         Welcome to your profile page!
-        <span className="p-2 ml-2 rounded-1xl bg-green-600">{params.id}</span>
+        <span className="p-2 ml-2 rounded-1xl bg-green-600">{id}</span>
       </p>
     </div>
   );
